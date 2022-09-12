ebd74c92c907201f319277011457264e9fa3d297
Boram Kim <boram2445@gmail.com>
2022-09-12 23:23:13 +0900

    Feat: 비디오 아이템 컴포넌트 생성

---
+++ src/components/videoItem/VideoItem.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import styles from "./videoItem.module.css";
+
+function VideoItem({ video }) {
+  const { thumbnails, title, channelId, channelTitle } = video;
+  const [channelImg, setChannelImg] = useState("");
+
+  async function getChannelImg() {
+    const BASE_URL = "https://www.googleapis.com/youtube/v3";
+    const API_KEY = process.env.REACT_APP_YOUTUBE_KEY;
+    const requestOptions = {
+      method: "GET",
+    };
+    const response = await fetch(
+      `${BASE_URL}/channels?part=snippet&id=${channelId}&fields=items%2Fsnippet%2Fthumbnails&key=${API_KEY}`,
+      requestOptions
+    );
+    const data = await response.json();
+    const img = data.items[0].snippet.thumbnails.default.url;
+    setChannelImg(img);
+  }
+  useEffect(() => {
+    getChannelImg();
+  }, []);
+  return (
+    <li className={styles["video-container"]}>
+      <div className={styles["thumb-img"]}>
+        <img alt={title} src={thumbnails.medium.url} />
+      </div>
+      <div className={styles["info-container"]}>
+        <div className={styles["channel-img"]}>
+          <img alt={channelTitle} src={channelImg} />
+        </div>
+        <div className={styles["right-wrapper"]}>
+          <strong className={styles["video-title"]}>{title}</strong>
+          <p className={styles["channel-title"]}>{channelTitle}</p>
+          <p className={styles["channel-info"]}>
+            조회수 3.3만회 &middot; 2시간 전
+          </p>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default VideoItem;
