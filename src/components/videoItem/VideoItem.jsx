721577dee3b9e54d60cdaef5d20759c1968840ff
Boram Kim <boram2445@gmail.com>
2022-09-13 15:59:07 +0900

    Design: video item 컴포넌트 배치 수정

--- src/components/videoItem/VideoItem.jsx
+++ src/components/videoItem/VideoItem.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import styles from "./videoItem.module.css";
+import { unescapeHtml } from "../../utils/unescapeHtml";
 
 function VideoItem({ video }) {
   const { thumbnails, title, channelId, channelTitle } = video;
+  const convertTitle = unescapeHtml(title);
   const [channelImg, setChannelImg] = useState("");
 
   async function getChannelImg() {
@@ -23,7 +25,7 @@ function VideoItem({ video }) {
     getChannelImg();
   }, []);
   return (
-    <li className={styles["video-container"]}>
+    <li className={styles["video-item"]}>
       <div className={styles["thumb-img"]}>
         <img alt={title} src={thumbnails.medium.url} />
       </div>
@@ -32,7 +34,7 @@ function VideoItem({ video }) {
           <img alt={channelTitle} src={channelImg} />
         </div>
         <div className={styles["right-wrapper"]}>
-          <strong className={styles["video-title"]}>{title}</strong>
+          <strong className={styles["video-title"]}>{convertTitle}</strong>
           <p className={styles["channel-title"]}>{channelTitle}</p>
           <p className={styles["channel-info"]}>
             조회수 3.3만회 &middot; 2시간 전
