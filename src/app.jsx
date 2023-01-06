2446c6a33d0560b156ed3f06e68202b394d9153c
Boram2445 <boram2445@gmail.com>
2023-01-06 11:56:58 +0900

    Refactor: 라우터 리팩토링

--- src/app.jsx
+++ src/app.jsx
@@ -1,34 +1,13 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./components/Root";
-import DetailPage from "./pages/DetailPage";
-import HomePage from "./pages/HomePage";
-import SearchPage from "./pages/SearchPage";
+import { Outlet } from 'react-router-dom';
+import SearchHeader from './components/SearchHeader'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <p>Sorry, not Found😂</p>,
-    children: [
-      { index: true, element: <HomePage /> },
-      {
-        path: "/search/:searchId",
-        element: <SearchPage />,
-      },
-      {
-        path: "/watch/:videoId",
-        element: <DetailPage />,
-      },
-      {
-        path: "/channel/:channelId",
-        element: <p>Channel</p>,
-      },
-    ],
-  },
-]);
-
-function App({ youtube }) {
-  return <RouterProvider router={router}></RouterProvider>;
+function App() {
+  return (
+    <div className="bg-black text-white pb-32">
+      <SearchHeader />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
 import "./styles/global.css";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Videos from './pages/Videos';
+import Search from './pages/Search';
+import VideoDetail from './pages/VideoDetail';
 
-// const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_KEY);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <p>Sorry, not Found😂</p>,
+    children: [
+      { index: true, element: <Videos /> },
+      {
+        path: "/videos",
+        element: <Videos />,
+      },
+      {
+        path: "/videos/:keyword",
+        element: <Videos />,
+      },
+      {
+        path: "/videos/watch/:videoId",
+        element: <VideoDetail />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>
 );
